bf869ccf02abc8bd442b1eec854f88593d98f946
Shajidaa <shajidaakterlopa101@gmail.com>
2025-11-10 00:39:25 +0600

    delete add bills method

--- index.js
+++ index.js
@@ -131,6 +131,12 @@ async function run() {
     });
     // ********* my bills delete ***********/
 
+    app.delete("/my-bills/:id", verifyFireBaseToken, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await myBillsCollection.deleteOne(query);
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
